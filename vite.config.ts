bbf993cde228c527659352658721555be0adc873
johypark97 <johypark97@gmail.com>
2026-03-14 13:08:22 +0900

    Update base path settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const baseHelper = (x?: string) => (x ? `${x}/` : "/");
+
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  base: baseHelper(process.env.VITE_BASE_PATH),
 });
