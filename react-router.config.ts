90cce049d0e24b6089eb3a5c04690297cd3cb379
johypark97 <johypark97@gmail.com>
2026-03-15 15:27:30 +0900

    add log to BASE_URL

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
+console.log(`import.meta.env.BASE_URL: ${import.meta.env.BASE_URL}`);
+
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
@@ -10,4 +12,4 @@ export default {
 // basename: process.env.VITE_BASE_PATH ?? "/", - OK
 // basename: process.env.VITE_BASE_PATH || "/", - OK 처음에는 안됐는데 왜 되는거야
 // no basename: - failed
-// import.meta.env.BASE_URL: ????
+// import.meta.env.BASE_URL: failed
