cc251632c724f894b04823a18fa79f58242093ca
johypark97 <johypark97@gmail.com>
2026-03-15 11:44:11 +0900

    no basename test

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,7 +4,7 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  basename: process.env.VITE_BASE_PATH || "/",
 } satisfies Config;
 
 // basename: process.env.VITE_BASE_PATH ?? "/", - OK
+// basename: process.env.VITE_BASE_PATH || "/", - OK 처음에는 안됐는데 왜 되는거야
