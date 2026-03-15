9925ecfe4cb9a596d9fee7cb0bae098676ab005c
johypark97 <johypark97@gmail.com>
2026-03-15 11:15:22 +0900

    remove basename config and fix workflows

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,11 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
-const basenameHelper = (path?: string) =>
-  !path || path === "/" ? "/" : `/${path.trim().replace(/^\/+|\/+$/g, "")}`;
-
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  basename: process.env.VITE_BASE_PATH,
 } satisfies Config;
