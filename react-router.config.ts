e3e7071e582fa56e4aabc2b24f833ff455839b88
johypark97 <johypark97@gmail.com>
2026-03-15 11:04:47 +0900

    update basename

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,11 @@
 import type { Config } from "@react-router/dev/config";
 
+const basenameHelper = (path?: string) =>
+  !path || path === "/" ? "/" : `/${path.trim().replace(/^\/+|\/+$/g, "")}`;
+
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  basename: "/test",
+  basename: basenameHelper(process.env.VITE_BASE_PATH),
 } satisfies Config;
