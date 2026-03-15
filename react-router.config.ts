4e667a92da3272596d0105099489dcc0c8a329cc
johypark97 <johypark97@gmail.com>
2026-03-15 15:32:47 +0900

    fix basename issue

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,15 +1,8 @@
 import type { Config } from "@react-router/dev/config";
 
-console.log(`import.meta.env.BASE_URL: ${import.meta.env.BASE_URL}`);
-
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  basename: import.meta.env.BASE_URL,
+  basename: process.env.VITE_BASE_PATH ?? "/",
 } satisfies Config;
-
-// basename: process.env.VITE_BASE_PATH ?? "/", - OK
-// basename: process.env.VITE_BASE_PATH || "/", - OK 처음에는 안됐는데 왜 되는거야
-// no basename: - failed
-// import.meta.env.BASE_URL: failed
